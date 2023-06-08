4e71948f0b113842c5a7a2c1adbfd930019ff374
Ken Adams <andrey.kalyshenko@gmail.com>
2023-06-08 08:33:26 +0300

    set up husky

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -3,7 +3,10 @@ import { mockedCurrency } from "./data";
 
 import { apiRouts } from "../../src/routes/apiRouts";
 
-export const { GET_CURRENCY_URL } = apiRouts;
+export const GET_CURRENCY_URL = apiRouts.GET_CURRENCY_URL.slice(
+  0,
+  apiRouts.GET_CURRENCY_URL.indexOf("?"),
+);
 
 export const handlers = [
   rest.get(GET_CURRENCY_URL, (req, res, ctx) => {
