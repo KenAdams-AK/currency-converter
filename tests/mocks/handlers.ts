14ed4b350f530029a0f87f07c9942e70e0d364b3
Ken Adams <andrey.kalyshenko@gmail.com>
2023-06-11 09:06:42 +0300

    fix test server handler routs

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -3,13 +3,13 @@ import { mockedCurrency } from "./data";
 
 import { apiRouts } from "../../src/routes/apiRouts";
 
-export const GET_CURRENCY_URL = apiRouts.GET_RATES_URL.slice(
-  0,
-  apiRouts.GET_RATES_URL.indexOf("?"),
-);
+// export const GET_CURRENCY_URL = apiRouts.GET_RATES_URL.slice(
+//   0,
+//   apiRouts.GET_RATES_URL.indexOf("?"),
+// );
 
 export const handlers = [
-  rest.get(GET_CURRENCY_URL, (_req, res, ctx) => {
+  rest.get(apiRouts.FETCH_CURRENCY_SYMBOLS, (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockedCurrency), ctx.delay(150));
   }),
 ];
