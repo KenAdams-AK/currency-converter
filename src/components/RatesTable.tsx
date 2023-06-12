7919f3207124a261d911b1144ebe160bfa1934c6
Ken Adams <andrey.kalyshenko@gmail.com>
2023-06-12 17:17:19 +0300

    fix production bug

--- src/components/RatesTable.tsx
+++ src/components/RatesTable.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import { Rates } from "../models/latestRates";
 
 type Props = {
@@ -8,19 +7,16 @@ type Props = {
 export default function RatesTable(props: Props) {
   const { rates } = props;
 
-  const ratesList = useMemo(() => Object.entries(rates), [rates]);
-
   return (
     <div className="RatesTable">
       <table className="RatesTable__table">
         <tbody>
-          {ratesList.length > 0 &&
-            ratesList.map((rate) => (
-              <tr key={rate[0]}>
-                <td>{rate[0]}</td>
-                <td>{rate[1]}</td>
-              </tr>
-            ))}
+          {Object.entries(rates).map((rate) => (
+            <tr key={rate[0]}>
+              <td>{rate[0]}</td>
+              <td>{rate[1]}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
